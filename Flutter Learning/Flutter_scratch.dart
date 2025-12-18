3bd0fefa21928e68cb64b1124963e38d8ffebfb6
vinayaka <vinayakah47@gmail.com>
2025-12-18 12:25:47 +0530

    Flutter learning about comments

--- Flutter Learning/Flutter_scratch.dart
+++ Flutter Learning/Flutter_scratch.dart
@@ -3,4 +3,13 @@ void main() {
   print('74' * 97);
   print('3' + '4');
   print(75 / 5 + 2);
+  print(75 / (5 + 2));
+  /* Dart follows Bodmas(Dart evaluates mathematical expression based on standard order of operations)
+
+  B= Brackets
+  O=Orders(squre, cube)
+  d=Division
+  M=Multiplication
+  A=Adition
+  S=Subtraction */
 }
